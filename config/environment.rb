6334f4f015a6f8d6f468f27602852142ac664000
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-07-18 11:55:27 +0100

    environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,43 @@
+###########################################
+###########################################
+##   _____                 __ _ _        ##
+##  |  __ \               / _(_) |       ##
+##  | |  \/ ___ _ __ ___ | |_ _| | ___   ##
+##  | | __ / _ \ '_ ` _ \|  _| | |/ _ \  ##
+##  | |_\ \  __/ | | | | | | | | |  __/  ##
+##  \_____/\___|_| |_| |_|_| |_|_|\___|  ##
+##                                       ##
+###########################################
+###########################################
+
+## Libs ##
+require 'bundler/setup'
+Bundler.require
+
+###########################################
+###########################################
+
+## Development ##
+configure :development do
+ ENV['SINATRA_ENV'] ||= "development"
+require 'bundler/setup'
+ Bundler.require(:default, ENV['SINATRA_ENV'])
+
+end
+
+## Production ##
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
+
+###########################################
+###########################################
