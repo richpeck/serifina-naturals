902addf49734be8a906347d3122f61f70b0ef15a
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-07-27 06:36:30 +0100

    Charm

--- app/app.rb
+++ app/app.rb
@@ -258,9 +258,9 @@ class SinatraApp < Sinatra::Base
 
     # => Items
     @shop    = Shop.find_by name: params[:shop]
-    @shape   = Shape.find params[:shape]
-    @charm   = @shape.charms.find params[:charm]
-    @stones  = Stone.find params[:stones]
+    @shape   = Shape.find         params[:shape]
+    @charm   = Charm.find params[:charm] #@shape.charms.find params[:charm]
+    @stones  = Stone.where id:    params[:stones]
 
     # => Session
     # => Because we're registering/logging in on behalf of the store, we need to ensure we are using their store
