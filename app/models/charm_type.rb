f1e435782ac0796f7365f284373201ae98f4c572
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-11-18 09:59:14 +0000

    Charm Type

--- app/models/charm_type.rb
+++ app/models/charm_type.rb
@@ -9,24 +9,13 @@
 ##                                        ##
 ############################################
 ############################################
-## Allows us to call charms and link them with the
-## various shapes, stones etc
+## Charm Types - should be enum but may wish to change
 ############################################
 ############################################
 
 ## Charm ##
-## id | charm_type | name | price | created_at | updated_at ##
-class Charm < ActiveRecord::Base
-
-  # => Charm Type
-  # => Gives us the ability to determine the type of the charm
-  enum charm_type: %i[dream_catcher ohms paisley steampunk_gear tree_of_life peace feather hamsa lotus chakra heart mini_heart_&_arrow lock_and_key owl paw_print tear_drop cross word starfish seahorse anchor turtle dolphin premium_mini seashell request_shape sm_med_or_large shark_teeth]
-
-  # => Associations
-  # => Gives us the ability to associate different objects
-  # => EG @shop.shapes.find(1).charms
-  has_many :associations, as: :associated, dependent: :destroy
-  has_many :shapes, through: :associations, source: :associatiable, source_type: "Shape"
+## id | name | created_at | updated_at ##
+class CharmType < ActiveRecord::Base
 
 end
 
