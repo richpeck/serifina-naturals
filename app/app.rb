6c20bd8c4aeea1b9d4eedb74bcf6d5a674dcec3a
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-11-19 11:42:43 +0000

    Charm Type

--- app/app.rb
+++ app/app.rb
@@ -238,7 +238,7 @@ class SinatraApp < Sinatra::Base
     # => Vars
     # => Allows us to identify the various variables for the system
     @shape  = Shape.find_by(id: params[:shape])
-    @charms = @shape.try(:charms).try(:includes, :charm_type).try(:select, "charms.id, charms.name, charms.price, charm_type.name AS charm_type").try(:order, "charms.charm_type_id" => :asc).try(:references, :charm_type) # Charm.all.joins(:charm_types).order("charm_type.name": :asc)
+    @charms = @shape.try(:charms).try(:includes, :charm_type).try(:select, "charms.id, charms.name, charms.price").try(:order, "charms.charm_type_id" => :asc).try(:references, :charm_type) # Charm.all.joins(:charm_types).order("charm_type.name": :asc)
 
     # => Build out response object
     # => This is designed to provide the user with a series of pieces of information
